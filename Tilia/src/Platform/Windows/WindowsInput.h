2f00b322e33c8fc98664dd771228089c14cbe5ba
WildWulf <egorken3v@gmail.com>
2022-03-14 01:37:31 +0200

    Add imput polling

---
+++ Tilia/src/Platform/Windows/WindowsInput.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Tilia/Input.h"
+
+namespace Tilia {
+
+	class WindowsInput : public Input
+	{
+	protected:
+		virtual bool IsKeyPressedImpl(int keycode) override;
+
+		virtual bool IsMouseButtonPressedImpl(int button) override;
+		virtual std::pair<float, float> GetMousePositionImpl() override;
+		virtual float GetMouseXImpl() override;
+		virtual float GetMouseYImpl() override;
+	};
+
+}
