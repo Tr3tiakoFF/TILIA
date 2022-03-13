255ea08f3283ae43ee1862d2dc25d09b9f91fa6d
WildWulf <egorken3v@gmail.com>
2022-03-14 01:28:19 +0200

    Add a window retrieval method

--- Tilia/src/Tilia/Core.h
+++ Tilia/src/Tilia/Core.h
@@ -15,11 +15,11 @@
 #endif
 
 #ifdef TL_ENABLE_ASSERTS
-#define TL_ASSERT(x, ...) { if(!(x)) { TL_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-#define TL_CORE_ASSERT(x, ...) { if(!(x)) { TL_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define TL_ASSERT(x, ...) { if(!(x)) { TL_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define TL_CORE_ASSERT(x, ...) { if(!(x)) { TL_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #else
-#define TL_ASSERT(x, ...)
-#define TL_CORE_ASSERT(x, ...)
+	#define TL_ASSERT(x, ...)
+	#define TL_CORE_ASSERT(x, ...)
 #endif
 
 
