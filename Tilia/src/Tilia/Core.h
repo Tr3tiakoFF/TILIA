bc5b6524fb79fef0568d5d8318763970f913c943
WildWulf <egorken3v@gmail.com>
2022-03-14 01:05:12 +0200

    Define TL_ENABLE_ASSERS and ASSERT log fix

--- Tilia/src/Tilia/Core.h
+++ Tilia/src/Tilia/Core.h
@@ -10,10 +10,13 @@
 	#error Tilia only support Windows! (for now)
 #endif
 
+#ifdef TL_DEBUG
+	#define TL_ENABLE_ASSERTS
+#endif
 
 #ifdef TL_ENABLE_ASSERTS
-#define TL_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-#define TL_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#define TL_ASSERT(x, ...) { if(!(x)) { TL_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#define TL_CORE_ASSERT(x, ...) { if(!(x)) { TL_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #else
 #define TL_ASSERT(x, ...)
 #define TL_CORE_ASSERT(x, ...)
